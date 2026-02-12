1c92d14f117568625460db6f754bffa9a287bee5
Arina Degtyareva <d.arina.bobr@gmail.com>
2026-02-12 22:50:34 +0300

    create task 14

---
+++ src/Mod2/DoubleArrays14.java
@@ -0,0 +1,43 @@
+package Mod2;
+/*
+Сформировать случайную матрицу m x n, состоящую из нулей и единиц, причем в каждом столбце число единиц равно номеру
+столбца.
+ */
+
+import java.util.Scanner;
+
+public class DoubleArrays14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int line = sc.nextInt();
+        int column = sc.nextInt();
+
+        int[][] matrice = new int[line][column];
+        for (int i = 0; i < line; i++) {
+            for (int j = 0; j < column; j++) {
+                matrice[i][j] = (int) (Math.random() * 2);
+            }
+        }
+
+        for (int j = 0; j < column; j++) {
+            int count = 0;
+            for (int i = 0; i < line; i++) {
+                if (count == j) {
+                    matrice[i][j] = 0;
+                }
+                if (count < j) {
+                    matrice[i][j] = 1;
+                }
+                if (count > j) {
+                    matrice[i][j] = 0;
+                }
+                if (matrice[i][j] == 1) {
+                    count++;
+                }
+                System.out.print(matrice[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
